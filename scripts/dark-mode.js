090433bd3627eae9c84ac310225d82e254b12469
mignrod <mignrod@gmail.com>
2024-05-08 09:47:48 -0400

    adding dark mode

---
+++ scripts/dark-mode.js
@@ -0,0 +1,22 @@
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+const nav = document.querySelector("nav a");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🌓︎")) {
+		body.style.background = "#000";
+		body.style.color = "#fff";
+        nav.style.color = "white";
+        header.style.background = "rgb(15, 97, 129)";
+        footer.style.background = "rgb(15, 97, 129)";
+		modeButton.textContent = "☼";
+	} else {
+		body.style.background = "#eee";
+		body.style.color = "#000";
+        header.style.background = "rgb(15, 97, 159)";
+        footer.style.background = "rgb(15, 97, 159)";
+		modeButton.textContent = "🌓︎";
+	}
+});
