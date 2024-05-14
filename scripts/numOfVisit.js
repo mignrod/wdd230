976339be889436ec231b7bceeb385d52bc31bc02
mignrod <mignrod@gmail.com>
2024-05-13 22:01:37 -0400

    activity team

---
+++ scripts/numOfVisit.js
@@ -0,0 +1,14 @@
+const visitNumber = document.querySelector('.visit');
+
+let numVisits = Number(window.localStorage.getItem('visitStorage')) || 0;
+
+if(numVisits !== 0) {
+    visitNumber.textContent = numVisits;
+
+} else {
+    visitNumber.textContent = `Hi! This is your first visit. Welcome!.`;
+};
+
+numVisits++;
+
+localStorage.getItem('visitStorage', numVisits);
