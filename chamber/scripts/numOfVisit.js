63dd83a9a1460b8785bcff72393a85d3971e938a
mignrod <mignrod@gmail.com>
2024-05-17 08:47:42 -0400

    Fixing calculations

--- chamber/scripts/numOfVisit.js
+++ chamber/scripts/numOfVisit.js
@@ -1,19 +1,18 @@
 const visitCounter = document.querySelector('.visit');
 
-const currentVisit = Number(Date.now());
-
+const currentVisit = Date.now();
 let numVisits = Number(window.localStorage.getItem('visitStorage')) || 0;
 const lastVisit = Number(window.localStorage.getItem('lastVisit'));
 
 const timePassed = ((currentVisit - lastVisit) / 86400000);
-const days = Math.round(timePassed / 24);
+const days = Math.floor(timePassed);
 
-if(numVisits !== 0 && timePassed < 24) {
+if(numVisits !== 0 && timePassed === 0) {
     visitCounter.textContent = `Back so soon! Awesome!`;
 
-} else if(numVisits !== 0 && timePassed < 48) {
+} else if(numVisits !== 0 && timePassed < 2) {
     visitCounter.textContent = `You last visited 1 day ago.`;
-} else if (numVisits !== 0 && timePassed > 48) {
+} else if (numVisits !== 0 && timePassed > 1) {
     visitCounter.textContent = `You last visited ${days} days ago.`;
 } else {
     visitCounter.textContent = `Welcome! Let us know if you have any questions.`;
