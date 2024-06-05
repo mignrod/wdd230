e8d924dacfa6698f3d47053c4b074685306e1eb7
mignrod <mignrod@gmail.com>
2024-06-04 23:28:55 -0400

    forecast finished

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,8 +1,8 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
+const figureCaption = document.querySelector('figcaption');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=10.07&lon=-69.32&units=metric&appid=2a5cb590ce7ab11fe3c5edabb6585178';
-const urlForecast = 'https://api.openweathermap.org/data/2.5/forecast?lat=10.07&lon=-69.32&units=metric&appid=2a5cb590ce7ab11fe3c5edabb6585178';
 
 async function apiFetch() {
     try {
@@ -10,7 +10,6 @@ async function apiFetch() {
         if(response.ok) {
             const data = await response.json();
             displayResults(data);
-            console.log(data);
             
         } else {
             throw Error(await response.text());
@@ -22,13 +21,13 @@ async function apiFetch() {
 
 function displayResults(data) {
     let desc = data.weather[0].description;
-    currentTemp.innerHTML = `${data.main.temp}&deg;C - ${desc.toUpperCase()}`;
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', 'Weather Icon');
-    weatherIcon.setAttribute('width', '50');
+    weatherIcon.setAttribute('width', '60');
     weatherIcon.setAttribute('title', desc);
-
+    figureCaption.textContent = desc.toUpperCase();
 }
 
 apiFetch();
