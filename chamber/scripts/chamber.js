ee1cb0cd045fc7a8479c0e97b0381214c685c244
mignrod <mignrod@gmail.com>
2024-06-05 22:50:44 -0400

    probando enlace

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -22,3 +22,9 @@ document.querySelector('#date').innerHTML = `&copy${currentDate.getFullYear()}`;
 let fileModification = new Date(document.lastModified);
 
 document.querySelector('#lastModified').innerHTML = `Last Modification: ${fileModification.toLocaleString('en-US')}`;
+
+// const joinButton = document.querySelector('.join');
+
+// joinButton.addEventListener('click', () => {
+    
+// })
